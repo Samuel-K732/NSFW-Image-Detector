294f9901254b20c93370e46c2b17e7dd29125c6d
Samuel <125740724+Samuel-K732@users.noreply.github.com>
2025-07-04 13:01:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# NSFW Image Detector (FastAPI + Sightengine)  
+
+Backend приложение, которое принимает .png или .jpg изображение и отправляет его в Sightengine. В итоге приложение сообщает содержится ли NSFW контент в предоставленном изображении.  
+NSFW параметры: nudity, gore, drugs.
+
+## Требования для запуска:
+- Python 3.10+
+- Sightengine api_user и api_secret. Для получения ключей нужно создать аккаунт по адресу: https://sightengine.com/ (Данные банковской карты не требуются!). 
+
+## Запуск:
+- Клонировать репозиторий ``git clone https://github.com/username/repo.git``
+- Установить зависимости ``pip install -r requirements.txt``
+- Cоздать .env файл со следующим содержимым:  
+``SIGHTENGINE_API_USER=ваш_user_id ``   
+``SIGHTENGINE_API_SECRET=ваш_secret_key``
+- Запуск сервера:  ``uvicorn main:app``  или ``python main.py``
+ http://localhost:5000/docs - Swagger документация.
+
+
+### Пример запроса через cURL:
+curl -X POST -F "file=@example.jpg" http://localhost:8000/moderate   
+ИЛИ (для Windows 10, 11)  
+curl.exe -X POST -F "file=@example.jpg" http://localhost:8000/moderate
+
+Возможные ответы:
+{ "status": "OK" } — изображение безопасно;  
+{ "status": "REJECTED", "reason": "NSFW content" } — найден нежелательный контент.
---
+++ main.py
@@ -0,0 +1,74 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import requests
+from dotenv import load_dotenv
+from os import getenv
+import uvicorn
+
+load_dotenv()
+
+app = FastAPI()
+
+SIGHTENGINE_API_USER = getenv("SIGHTENGINE_API_USER")
+SIGHTENGINE_API_SECRET = getenv("SIGHTENGINE_API_SECRET")
+SIGHTENGINE_ENDPOINT = "https://api.sightengine.com/1.0/check.json"
+
+ALLOWED_TYPES = {"image/jpeg", "image/png"}
+
+
+@app.post("/moderate")
+async def moderate_image(file: UploadFile = File(...)):
+    if file.content_type not in ALLOWED_TYPES:
+        raise HTTPException(
+            status_code=400, detail="Данный формат файла не поддерживается"
+        )
+
+    params = {
+        "models": "nudity-2.1,recreational_drug,medical,gore-2.0,self-harm",
+        "api_user": SIGHTENGINE_API_USER,
+        "api_secret": SIGHTENGINE_API_SECRET,
+    }
+
+    response = requests.post(
+        SIGHTENGINE_ENDPOINT,
+        files={"media": (file.filename, file.file, file.content_type)},
+        data=params,
+    )
+
+    print("Status Code:", response.status_code)
+    print("Response Text:", response.text)
+
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Ошибка при запросе к API: {response.status_code} {response.text}",
+        )
+
+    data = response.json()
+
+    if nsfw_check(data):
+        return {"status": "REJECTED", "reason": "NSFW content"}
+    else:
+        return {"status": "OK"}
+
+
+def nsfw_check(data: dict) -> bool:
+    nudity_keys = (
+        "sexual_activity",
+        "sexual_display",
+        "erotica",
+        "very_suggestive",
+        "suggestive",
+        "mildly_suggestive",
+    )
+
+    nudity_score = max(data["nudity"][k] for k in nudity_keys)
+    drug_score = data["recreational_drug"]["prob"]
+    gore_score = data["gore"]["prob"]
+
+    scores = [nudity_score, drug_score, gore_score]
+
+    return any(score >= 0.7 for score in scores)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=5000)
